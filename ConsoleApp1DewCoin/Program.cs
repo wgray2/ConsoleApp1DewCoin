5258a58a185008610423ad1440faa80382de2485
Wyatt <wyattgray03@gmail.com>
2021-02-18 16:58:22 -0700

    Add project files.

---
+++ ConsoleApp1DewCoin/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace ConsoleApp1DewCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrivateKey key1 = new PrivateKey();
+            PublicKey wallet1 = key1.publicKey();
+
+            PrivateKey key2 = new PrivateKey();
+            PublicKey wallet2 = key2.publicKey();
+
+            Blockchain dewcoin = new Blockchain(3, 100);
+
+            Console.WriteLine("Start the miner");
+            dewcoin.MinePendingTransactions(wallet1);
+            Console.WriteLine("\nBalance of wallet is $" + dewcoin.GetBalanceOfWallet(wallet1).ToString());
+
+            Transaction tx1 = new Transaction(wallet1, wallet2, 10);
+            tx1.SignTransaction(key1);
+            dewcoin.addPendingTransaction(tx1);
+            Console.WriteLine("Start the miner");
+            dewcoin.MinePendingTransactions(wallet2);
+            Console.WriteLine("\nBalance of wallet1 is $" + dewcoin.GetBalanceOfWallet(wallet1).ToString());
+            Console.WriteLine("\nBalance of wallet2 is $" + dewcoin.GetBalanceOfWallet(wallet2).ToString());
+
+            string blockJSON = JsonConvert.SerializeObject(dewcoin, Formatting.Indented);
+            Console.WriteLine(blockJSON);
+
+
+
+            if (dewcoin.IsChainValid())
+            {
+                Console.WriteLine("Blockchain is valid");
+            }
+            else
+            {
+                Console.WriteLine("Blockchain not valid");
+            }
+                
+        }
+    }
+
+    
+
+    
+}
---
+++ ConsoleApp1DewCoin/Transaction.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace ConsoleApp1DewCoin
+{
+    class Transaction
+    {
+        public PublicKey FromAddress { get; set; }
+        public PublicKey ToAddress { get; set; }
+        public decimal Amount { get; set; }
+        public Signature Signature { get; set; }
+        public Transaction(PublicKey fromAddress, PublicKey toAddress, decimal amount)
+        {
+            this.FromAddress = fromAddress;
+            this.ToAddress = toAddress;
+            this.Amount = amount;
+        }
+
+        public void SignTransaction(PrivateKey signingKey)
+        {
+            string fromAddressDER = BitConverter.ToString(FromAddress.toDer()).Replace("-", "");
+            string signingDER = BitConverter.ToString(signingKey.publicKey().toDer()).Replace("-", "");
+
+            if(fromAddressDER != signingDER)
+            {
+                throw new Exception("You cannot sign transactions for other wallet!");
+            }
+            string txHash = this.CalculateHash();
+            this.Signature = Ecdsa.sign(txHash, signingKey);
+        }
+
+        public string CalculateHash()
+        {
+            string fromAddressDER = BitConverter.ToString(FromAddress.toDer()).Replace("-", "");
+            string toAddressDER = BitConverter.ToString(ToAddress.toDer()).Replace("-", "");
+            string transactionData = fromAddressDER + toAddressDER + Amount;
+            byte[] tdBytes = Encoding.ASCII.GetBytes(transactionData);
+            return BitConverter.ToString(SHA256.Create().ComputeHash(tdBytes)).Replace("-", "");
+        }
+
+        public bool IsValid()
+        {
+            if (this.FromAddress is null) return true;
+            if (this.Signature is null)
+            {
+                throw new Exception("No signature is in this transaction");
+            }
+            return Ecdsa.verify(this.CalculateHash(), this.Signature, this.FromAddress);
+        }
+    }
+}
